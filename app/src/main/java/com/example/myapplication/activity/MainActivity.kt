29b5347e4a20f5f938241bcf5fc6ca116d9b0795
TT <deyetiantian_i@didiglobal.com>
2022-01-26 10:54:31 +0800

    Third Commit

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -56,6 +57,7 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
             R.id.send -> {
                 val inputText = input_message.text.toString()
                 Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show()
+                Log.d("MainActivity","哈哈哈哈哈哈哈哈哈哈😝")
             }
             R.id.button3 ->{
                 val intent = Intent(this, FirstActivity::class.java)
